2df90d9e605b41267aee06212596c2cf2512f76f
phogatheena <109434462+phogatheena@users.noreply.github.com>
2024-01-10 23:31:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,16 +3,15 @@ function firstWord(s) {
 	let a =" ";
 	for(let i=0;i<s.length;i++)
 		{
-			if(s.charAt(i)===" ")
+			if(s.charAt(i)==" ")
 			{
 				return a;
 			}
 				a+=s.charAt(i);
 		}
-
+	return a;
 
 }
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
