2958bebf513bb74bfc31141b5d053551fbb88c69
phogatheena <109434462+phogatheena@users.noreply.github.com>
2024-01-10 23:12:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function firstWord(s) {
   // your code here
+	let a ="";
+	for(let i=0;i<s.length;i++)
+		{
+			if(s[i]==" ")
+			{
+				break;
+			}
+				a+=s[i];
+		}
+	return a;
+	
 }
-
 // Do not change the code below
 
 const s = prompt("Enter String:");
